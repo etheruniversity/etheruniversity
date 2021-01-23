f5accd7db0b6e821af72969bdb23b6dee81522d6
nathanhleung <github@nathanhleung.com>
2021-01-23 11:52:59 -0500

    feat: improve copy in account modal

--- src/components/AccountButton.tsx
+++ src/components/AccountButton.tsx
@@ -28,17 +28,19 @@ const AccountButton: React.FC<AccountButtonProps> = props => {
         <Modal isOpen={accountModalIsOpen} setIsOpen={setAccountModalIsOpen}>
           <H2 style={{ marginTop: 0 }}>Account Info</H2>
           <p>
-            Your address is <b>{address}</b>.
+            We've created an account for you on the{" "}
+            <b>{capitalize(TESTNET_NAME)}</b> Ethereum testnet.
           </p>
           <p>
-            You balance is <b>{web3?.utils.fromWei(balance) ?? 0} ETH</b>.
+            Your testnet address is <b>{address}</b>.
           </p>
           <p>
-            You are on the <b>{capitalize(TESTNET_NAME)}</b> testnet.
+            You testnet balance is{" "}
+            <b>{web3?.utils.fromWei(balance) ?? 0} ETH</b>.
           </p>
           <div>
             <ButtonLink to={`${ETHERSCAN_ENDPOINT}/address/${address}`}>
-              View Address Details
+              View Details on Etherscan
             </ButtonLink>
           </div>
         </Modal>
