78528a7bd39747ff39d0a54ade399e0941dded15
Evan Mays <me@evanmays.com>
2021-01-22 23:03:55 -0500

    Approving and depositing works. UI also loads initial balances. UI doesn't autoupdate. And approving and depositing don't display progress to user

--- src/pages/compound101.tsx
+++ src/pages/compound101.tsx
@@ -5,23 +5,42 @@ import ButtonLink from "ethereum-org-website/src/components/ButtonLink"
 import { useWeb3, useAccount} from "../hooks"
 
 // Source: https://github.com/compound-finance/compound-config
-const CUSDC_MAINNET_ADDRESS = "0x39AA39c021dfbaE8faC545936693aC917d5E7563";
+const ADDRESS_OBJ = {
+  MAINNET: {
+    CUSDC: "0x39AA39c021dfbaE8faC545936693aC917d5E7563",
+    USDC: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+  },
+  ROPSTEN: {
+    CUSDC: "0x8af93cae804cc220d1a608d4fa54d1b6ca5eb361",
+    USDC: "0x0D9C8723B343A8368BebE0B5E89273fF8D712e3C"
+  },
+  GOERLI: {
+    CUSDC: "0xcec4a43ebb02f9b80916f1c718338169d6d5c1f0",
+    USDC: "0xd87ba7a50b2e7e660f678a895e4b72e7cb4ccd9c"
+  }
+};
+
+const ADDRESS = ADDRESS_OBJ.GOERLI;
+
 const CUSDC_ABI = require('../ABIs/cUSDC_ABI.json');
 const CUSDC_DECIMALS = 1e18;
-const BLOCKS_PER_YEAR = 4 * 60 * 24 * 365; // based on 4 blocks occurring every minute
-const USDC_MAINNET_ADDRESS = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 const USDC_ABI = require('../ABIs/ERC20_ABI.json');
 const USDC_DECIMALS = 1e6;
+
+const BLOCKS_PER_YEAR = 4 * 60 * 24 * 365; // based on 4 blocks occurring every minute
 
 const Compound101 = () => {
   // I realize there's a bug here if a user deposits more than once. Let's not worry about that yet :)
   const [currentWalletUSDCBalance, setCurrentWalletUSDCBalance] = useState(0.0);
   const [currentWalletETHBalance, setCurrentWalletETHBalance] = useState(0.0);
+  const [currentAllowance, setCurrentAllowance] = useState(0.0);
   const [currentUSDCApy, setCurrentUSDCApy] = useState(0.0);
   const [averageTraditionalBankAPY, setAverageTraditionalBankAPY] = useState(0.0);
-  const [deposit, setDeposit] = useState({block: 0, amount: 0});
+  const [depositBlock, setDepositBlock] = useState(0.0);
+  const [depositAmount, setDepositAmount] = useState(0.0);
+  const [usdcPerCusdcRate, setUsdcPerCusdcRate] = useState(0.0);
   const [earnings, setEarnings] = useState({block: 0, usdcEarned: 0, compEarned: 0});
-  const depositButton = <ButtonLink to="#">Deposit All USDC</ButtonLink>
+  const depositButton = <div onClick={()=>{console.log("HI");depositButtonHandler(account, web3, Math.floor(currentWalletUSDCBalance))}}><ButtonLink to="#" isSecondary={currentAllowance < currentWalletUSDCBalance - .1}>Deposit All USDC</ButtonLink></div>
   const withdrawButton = <ButtonLink to="#">Withdraw All USDC</ButtonLink>
 
   const { web3, loading: web3loading } = useWeb3();
@@ -31,7 +50,7 @@ const Compound101 = () => {
     if (web3loading || !web3) {
       return
     }
-    const cUSDCContract = new web3.eth.Contract(CUSDC_ABI, CUSDC_MAINNET_ADDRESS);
+    const cUSDCContract = new web3.eth.Contract(CUSDC_ABI, ADDRESS.CUSDC);
     cUSDCContract.methods.supplyRatePerBlock().call().then((ratePerBlock) => {
       const growthPerBlock = 1.0 + parseFloat(ratePerBlock) / CUSDC_DECIMALS
       const usdcApy = 100 * (Math.pow(growthPerBlock, BLOCKS_PER_YEAR) - 1)
@@ -41,9 +60,20 @@ const Compound101 = () => {
       return
     }
     web3.eth.getBalance(account.address).then(parseFloat).then(a => a / 1e18).then(setCurrentWalletETHBalance);
-    const USDCContract = new web3.eth.Contract(USDC_ABI, USDC_MAINNET_ADDRESS);
+
+    const USDCContract = new web3.eth.Contract(USDC_ABI, ADDRESS.USDC);
     USDCContract.methods.balanceOf(account.address).call().then(parseFloat).then(a => a / USDC_DECIMALS).then(setCurrentWalletUSDCBalance);
-  });
+    USDCContract.methods.allowance(account.address, ADDRESS.CUSDC).call().then(parseFloat).then(a => a / USDC_DECIMALS).then(setCurrentAllowance);
+
+    const CUSDCContract = new web3.eth.Contract(CUSDC_ABI, ADDRESS.CUSDC);
+    CUSDCContract.methods.balanceOf(account.address).call().then(parseFloat).then(a => a / CUSDC_DECIMALS).then(setDepositAmount);
+
+    const updateExchangeRate = () => {
+      CUSDCContract.methods.exchangeRateCurrent().call().then(parseFloat).then(a => a / USDC_DECIMALS).then(setUsdcPerCusdcRate);
+      setTimeout(updateExchangeRate, 10000) // Poll indefinitely
+    }
+    updateExchangeRate(); // Start polling
+  }, [web3loading, web3, accountLoading, account]);
 
   return (
     <Layout>
@@ -54,12 +84,12 @@ const Compound101 = () => {
       </div>
       <p>The Compound protocol lets you deposit cryptoassets like ETH and USDC. You'll be rewarded for these deposits by earning inteest, similar to a savings account at your traditional bank. The Compound Protocol often gives higher interest rates than your bank. Sometimes, these interest rates get as high as 20% APY. Meanwhile, the average traditional bank savings account in the US currenlty only pays {averageTraditionalBankAPY.toFixed(2)}%. Compound also lets you take out loans, but we'll get to that in another quest.</p>
       <p>First, you'll need to give the Compound protocol the ability to withdraw your funds.</p>
-      <div style={{textAlign: "center", marginBottom: 20}}><ButtonLink to="#">Approve</ButtonLink></div>
+      <div style={{textAlign: "center", marginBottom: 20}} onClick={()=>{console.log("HI");approveButtonHandler(account, web3)}}><ButtonLink to="#">Approve</ButtonLink></div>
       <p>Awesome, now we can get to the real fun. Compound lets you earn interest on many types of cryptoassets. In this case, we'll deposit USDC. 1 USDC = $1 USD, so it's easier to compare to a traditional savings account. Once your USDC is deposited in Compound, you can't send it to others. The USDC is "locked" in Compound until you withdraw it.</p>
       <InteractionCard title="Compound Deposit" sideTextTitle="Your Wallet" sideTextBody={<span>USDC Balance: {currentWalletUSDCBalance.toFixed(2)}<br/>ETH Balance: {currentWalletETHBalance.toFixed(2)}</span>} circleText={<span>APY {currentUSDCApy.toFixed(2)}%</span>} button={depositButton} />
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac ut consequat semper viverra nam. Arcu bibendum at varius vel pharetra vel turpis nunc. Quis viverra nibh cras pulvinar mattis. Tempus egestas sed sed risus pretium. Eros in cursus turpis massa tincidunt dui ut ornare. Cras sed felis eget velit. Consectetur libero id faucibus nisl tincidunt eget nullam non. Tellus id interdum velit laoreet. Morbi blandit cursus risus at ultrices.</p>
-      <p>You earn interest every block. In the {earnings.block - deposit.block} blocks since you deposited, your balance has grown {earnings.usdcEarned} USDC, from {deposit.amount} USDC to {deposit.amount+earnings.usdcEarned} USDC. You’ve also earned {earnings.compEarned} COMP, the governance token. You can do xyz with the token, or exchange it for USDC on Uniswap.</p>
-      <InteractionCard title="Compound Deposit" sideTextTitle="Time Since Deposit" sideTextBody={<span>{earnings.block - deposit.block} blocks</span>} circleText={<span>BALANCE ${deposit.amount+earnings.usdcEarned}</span>} button={withdrawButton} />
+      <p>You earn interest every block. In the {earnings.block - depositBlock} blocks since you deposited, your balance has grown {earnings.usdcEarned} USDC, from {depositAmount * usdcPerCusdcRate} USDC to {depositAmount+earnings.usdcEarned} USDC. You’ve also earned {earnings.compEarned} COMP, the governance token. You can do xyz with the token, or exchange it for USDC on Uniswap.</p>
+      <InteractionCard title="Compound Deposit" sideTextTitle="Time Since Deposit" sideTextBody={<span>{earnings.block - depositBlock} blocks</span>} circleText={<span>BALANCE <span style={{fontSize: 16}}>${/*(depositAmount+earnings.usdcEarned)*/(depositAmount * usdcPerCusdcRate).toString()}</span></span>} button={withdrawButton} />
       <p>You might be wondering where your interest income is coming from. In a traditional bank savings account, the bank lends out your money to other people for them to buy homes. You're money is safe because if those people stop paying, the bank takes back their house and sells it. The Compound Protocol works in a similar fashion. But, it has stricter rules about when it lends out money and what happens if a borrower doesn't pay their loan. using youThis section is all about the technical TODO, talk about the asset pools and collaterlaization ratios.</p>
       <Link to="#">Dive deep into how Compound works</Link>
     </Layout>
@@ -72,6 +102,44 @@ const headerStyle = {
   justifyContent: "space-between",
   alignItems: "center",
   flexWrap: "wrap"
+}
+
+const signTx = (account, web3, tx) => {
+  account.signTransaction(tx).then((signedTx) => signedTx.rawTransaction).then((rawTx) => {
+    web3.eth.sendSignedTransaction(rawTx, (error, result) => {
+      if (error) {
+        console.error("Transaction failed");
+        console.error(error);
+      } else {
+        console.log("Transaction succeeded");
+      }
+    }).then(console.log);
+  });
+}
+const depositButtonHandler = (account, web3, amount) => {
+  const CUSDCContract = new web3.eth.Contract(CUSDC_ABI, ADDRESS.CUSDC);
+  CUSDCContract.methods.mint(1 * USDC_DECIMALS).call().then(console.log);
+  const tx = {
+    from: account.address,
+    to: ADDRESS.CUSDC,
+    data: CUSDCContract.methods.mint(1 * USDC_DECIMALS).encodeABI(), //amount * USDC_DECIMALS
+    gasPrice: 1,
+    gas: 300000
+  };
+  signTx(account, web3, tx)
+}
+
+const approveButtonHandler = (account, web3) => {
+  const USDCContract = new web3.eth.Contract(USDC_ABI, ADDRESS.USDC);
+  const tx = {
+    from: account.address,
+    to: ADDRESS.USDC,
+    data: USDCContract.methods.approve(ADDRESS.CUSDC, 1e3 * USDC_DECIMALS).encodeABI(),
+    gasPrice: 1,
+    gas: 300000
+  };
+  USDCContract.methods.approve(ADDRESS.CUSDC, 1e6 * USDC_DECIMALS).call().then(console.log);
+  signTx(account, web3, tx)
 }
 
 export default Compound101
